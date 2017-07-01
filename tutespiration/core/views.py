6d74fdc19cc9d2e678f847c420e4b9d8f4a29f7e
Andromeda Yelton <andromeda.yelton@gmail.com>
2017-07-01 14:29:55 -0400

    Make a (nonfunctioning) page that looks like the MIT libraries

--- tutespiration/core/views.py
+++ tutespiration/core/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class Tutespiration(TemplateView):
+    template_name = 'tutespiration.html'
--- tutespiration/urls.py
+++ tutespiration/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from tutespiration.core.views import Tutespiration
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'tutespiration.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', Tutespiration.as_view()),
 ]
